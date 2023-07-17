6b05b1ceae44deb5f84cb65006a441e4b2b98517
Shinigami92 <chrissi92@hotmail.de>
2023-07-17 19:56:02 +0200

    chore: convert to esm

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { buildSync } from 'esbuild';
-import { globSync } from 'glob';
 import { allLocales } from '../src';
 
 console.log('Building dist for node (cjs)...');
@@ -9,14 +8,12 @@ console.log('Building dist for node (cjs)...');
 const target = ['ES2019', 'node14.17'];
 
 buildSync({
-  entryPoints: globSync('./src/**/*.ts'),
-  // We can use the following entry points when esbuild supports cjs+splitting
-  // entryPoints: [
-  //   './src/index.ts',
-  //   ...Object.keys(locales).map((locale) => `./src/locale/${locale}.ts`),
-  // ],
+  entryPoints: [
+    './src/index.ts',
+    ...Object.keys(allLocales).map((locale) => `./src/locale/${locale}.ts`),
+  ],
   outdir: './dist/cjs',
-  bundle: false, // Creates 390MiB bundle ...
+  bundle: true, // Creates 49MiB bundle ...
   sourcemap: false,
   minify: true,
   // splitting: true, // Doesn't work with cjs
@@ -38,5 +35,4 @@ buildSync({
   splitting: true,
   format: 'esm',
   target,
-  outExtension: { '.js': '.mjs' },
 });
