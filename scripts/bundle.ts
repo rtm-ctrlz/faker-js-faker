suyashgulati/faker

infra!: drop NodeJS v14 support

scripts/bundle.ts
@@ -7,7 +7,7 @@ console.log('Building dist for node (cjs)...');
 
 // Generate entry-points for cjs compatibility
 const localeDir = 'locale';
-const target = ['ES2019', 'node14.17'];
+const target = ['ES2020', 'node16.13'];
 
 if (existsSync(localeDir)) {
   rmSync(localeDir, { recursive: true, force: true });

rtm-ctrlz/faker-js-faker

scripts/bundle.ts
@@ -6,7 +6,7 @@ import { allLocales } from '../src';
 
 console.log('Building dist for node (cjs)...');
 
-const target = ['ES2019', 'node14.17'];
+const target = ['ES2022', 'node16.13'];
 
 buildSync({
   entryPoints: globSync('./src/**/*.ts'),